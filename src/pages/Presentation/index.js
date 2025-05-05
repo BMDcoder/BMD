a1a6bc115a766b53e305ac0eef8d3ff7b5791c7e
BMDcoder <mwitabryson@gmail.com>
2025-05-05 17:00:26 +0300

    Update index.js

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -93,8 +93,7 @@ function Presentation() {
               px={{ xs: 6, lg: 12 }}
               mt={1}
             >
-              We are a next-gen marketing agency using artificial intelligence
-               We are a next-gen marketing agency using artificial intelligence
+              We are a next-gen marketing agency using artificial intelligence We are a next-gen marketing agency using artificial intelligence
             </MKTypography>
           </Grid>
         </Container>
