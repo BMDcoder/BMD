ee6675a3b5f245a9bcdd7b966efe3c3ea0963b1b
BMDcoder <mwitabryson@gmail.com>
2025-05-05 17:58:32 +0300

    x

--- src/pages/Presentation/index.js
+++ src/pages/Presentation/index.js
@@ -93,7 +93,7 @@ function Presentation() {
               px={{ xs: 6, lg: 12 }}
               mt={1}
             >
-               We are a next-gen marketing agency using artificial intelligence to craft data-backed, high-performing marketing strategies
+               Smart marketing, powered by AI and data.
             </MKTypography>
           </Grid>
         </Container>
