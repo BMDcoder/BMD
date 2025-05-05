85b94962e68574319b5b332db38beb08051d2f8f
BMDcoder <mwitabryson@gmail.com>
2025-05-05 18:51:03 +0300

    x

--- src/pages/Presentation/sections/Counters.js
+++ src/pages/Presentation/sections/Counters.js
@@ -32,7 +32,7 @@ function Counters() {
           <Grid item xs={12} md={4}>
             <DefaultCounterCard
               //count={70}
-              suffix="+"
+              //suffix="+"
               title="Coded Elements"
               description="From buttons, to inputs, navbars, alerts or cards, you are covered"
             />
@@ -40,8 +40,8 @@ function Counters() {
           <Grid item xs={12} md={4} display="flex">
             <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
             <DefaultCounterCard
-              count={15}
-              suffix="+"
+              //count={15}
+              //suffix="+"
               title="Design Blocks"
               description="Mix the sections, change the colors and unleash your creativity"
             />
@@ -49,8 +49,8 @@ function Counters() {
           </Grid>
           <Grid item xs={12} md={4}>
             <DefaultCounterCard
-              count={4}
-              title="Pages"
+              //count={4}
+              //title="Pages"
               description="Save 3-4 weeks of work when you use our pre-made pages for your website"
             />
           </Grid>
