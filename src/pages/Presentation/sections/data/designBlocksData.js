4f1015cd66e42c2f605c212ec7d51ea7896b27df
BMDcoder <mwitabryson@gmail.com>
2025-05-10 15:30:50 +0300

    i

--- src/pages/Presentation/sections/data/designBlocksData.js
+++ src/pages/Presentation/sections/data/designBlocksData.js
@@ -1,84 +1,248 @@
-/*
-=========================================================
-* Material Kit 2 React - v2.1.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-kit-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
-const localImage = "src/assets/images/logo.png";
+import localImage from 'assets/images/logo.png';
 
 export default [
   {
-    title: "Design Blocks",
-    description: "A selection of 45 page sections that fit perfectly in any combination",
+    title: 'Design Blocks',
+    description:
+      'A selection of 45 page sections that fit perfectly in any combination',
     items: [
-      { image: localImage, name: "Page Headers", count: 10, route: "/sections/page-sections/page-headers" },
-      { image: localImage, name: "Features", count: 14, route: "/sections/page-sections/features" },
-      { image: localImage, name: "Pricing", count: 8, pro: true },
-      { image: localImage, name: "FAQ", count: 1, pro: true },
-      { image: localImage, name: "Blog Posts", count: 11, pro: true },
-      { image: localImage, name: "Testimonials", count: 11, pro: true },
-      { image: localImage, name: "Teams", count: 6, pro: true },
-      { image: localImage, name: "Stats", count: 3, pro: true },
-      { image: localImage, name: "Call to Actions", count: 8, pro: true },
-      { image: localImage, name: "Applications", count: 6, pro: true },
-      { image: localImage, name: "Logo Areas", count: 4, pro: true },
-      { image: localImage, name: "Footers", count: 10, pro: true },
-      { image: localImage, name: "General Cards", count: 9, pro: true },
-      { image: localImage, name: "Content Sections", count: 8, pro: true },
+      {
+        image: localImage,
+        name: 'Page Headers',
+        count: 10,
+        route: '/sections/page-sections/page-headers',
+      },
+      {
+        image: localImage,
+        name: 'Features',
+        count: 14,
+        route: '/sections/page-sections/features',
+      },
+      {
+        image: localImage,
+        name: 'Pricing',
+        count: 8,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'FAQ',
+        count: 1,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Blog Posts',
+        count: 11,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Testimonials',
+        count: 11,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Teams',
+        count: 6,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Stats',
+        count: 3,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Call to Actions',
+        count: 8,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Applications',
+        count: 6,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Logo Areas',
+        count: 4,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Footers',
+        count: 10,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'General Cards',
+        count: 9,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Content Sections',
+        count: 8,
+        pro: true,
+      },
     ],
   },
   {
-    title: "Navigation",
-    description: "30+ components that will help go through the pages",
+    title: 'Navigation',
+    description: '30+ components that will help go through the pages',
     items: [
-      { image: localImage, name: "Navbars", count: 4, route: "/sections/navigation/navbars" },
-      { image: localImage, name: "Nav Tabs", count: 2, route: "/sections/navigation/nav-tabs" },
-      { image: localImage, name: "Pagination", count: 3, route: "/sections/navigation/pagination" },
+      {
+        image: localImage,
+        name: 'Navbars',
+        count: 4,
+        route: '/sections/navigation/navbars',
+      },
+      {
+        image: localImage,
+        name: 'Nav Tabs',
+        count: 2,
+        route: '/sections/navigation/nav-tabs',
+      },
+      {
+        image: localImage,
+        name: 'Pagination',
+        count: 3,
+        route: '/sections/navigation/pagination',
+      },
     ],
   },
   {
-    title: "Input Areas",
-    description: "50+ elements that you need for text manipulation and insertion",
+    title: 'Input Areas',
+    description:
+      '50+ elements that you need for text manipulation and insertion',
     items: [
-      { image: localImage, name: "Newsletters", count: 6, pro: true },
-      { image: localImage, name: "Contact Sections", count: 8, pro: true },
-      { image: localImage, name: "Forms", count: 3, route: "/sections/input-areas/forms" },
-      { image: localImage, name: "Inputs", count: 6, route: "/sections/input-areas/inputs" },
+      {
+        image: localImage,
+        name: 'Newsletters',
+        count: 6,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Contact Sections',
+        count: 8,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Forms',
+        count: 3,
+        route: '/sections/input-areas/forms',
+      },
+      {
+        image: localImage,
+        name: 'Inputs',
+        count: 6,
+        route: '/sections/input-areas/inputs',
+      },
     ],
   },
   {
-    title: "Attention Catchers",
-    description: "20+ Fully coded components that popup from different places of the screen",
+    title: 'Attention Catchers',
+    description:
+      '20+ Fully coded components that popup from different places of the screen',
     items: [
-      { image: localImage, name: "Alerts", count: 4, route: "/sections/attention-catchers/alerts" },
-      { image: localImage, name: "Notifications", count: 3, pro: true },
-      { image: localImage, name: "Tooltips & Popovers", count: 2, route: "/sections/attention-catchers/tooltips-popovers" },
-      { image: localImage, name: "Modals", count: 5, route: "/sections/attention-catchers/modals" },
+      {
+        image: localImage,
+        name: 'Alerts',
+        count: 4,
+        route: '/sections/attention-catchers/alerts',
+      },
+      {
+        image: localImage,
+        name: 'Notifications',
+        count: 3,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Tooltips & Popovers',
+        count: 2,
+        route: '/sections/attention-catchers/tooltips-popovers',
+      },
+      {
+        image: localImage,
+        name: 'Modals',
+        count: 5,
+        route: '/sections/attention-catchers/modals',
+      },
     ],
   },
   {
-    title: "Elements",
-    description: "80+ carefully crafted small elements that come with multiple colors and shapes",
+    title: 'Elements',
+    description:
+      '80+ carefully crafted small elements that come with multiple colors and shapes',
     items: [
-      { image: localImage, name: "Buttons", count: 6, route: "/sections/elements/buttons" },
-      { image: localImage, name: "Avatars", count: 2, route: "/sections/elements/avatars" },
-      { image: localImage, name: "Dropdowns", count: 2, route: "/sections/elements/dropdowns" },
-      { image: localImage, name: "Toggles", count: 2, route: "/sections/elements/toggles" },
-      { image: localImage, name: "Social Buttons", count: 2, pro: true },
-      { image: localImage, name: "Breadcrumbs", count: 1, route: "/sections/elements/breadcrumbs" },
-      { image: localImage, name: "Badges", count: 3, route: "/sections/elements/badges" },
-      { image: localImage, name: "Progress Bars", count: 4, route: "/sections/elements/progress-bars" },
-      { image: localImage, name: "Tables", count: 3, pro: true },
-      { image: localImage, name: "Typography", count: 2, route: "/sections/elements/typography" },
+      {
+        image: localImage,
+        name: 'Buttons',
+        count: 6,
+        route: '/sections/elements/buttons',
+      },
+      {
+        image: localImage,
+        name: 'Avatars',
+        count: 2,
+        route: '/sections/elements/avatars',
+      },
+      {
+        image: localImage,
+        name: 'Dropdowns',
+        count: 2,
+        route: '/sections/elements/dropdowns',
+      },
+      {
+        image: localImage,
+        name: 'Toggles',
+        count: 2,
+        route: '/sections/elements/toggles',
+      },
+      {
+        image: localImage,
+        name: 'Social Buttons',
+        count: 2,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Breadcrumbs',
+        count: 1,
+        route: '/sections/elements/breadcrumbs',
+      },
+      {
+        image: localImage,
+        name: 'Badges',
+        count: 3,
+        route: '/sections/elements/badges',
+      },
+      {
+        image: localImage,
+        name: 'Progress Bars',
+        count: 4,
+        route: '/sections/elements/progress-bars',
+      },
+      {
+        image: localImage,
+        name: 'Tables',
+        count: 3,
+        pro: true,
+      },
+      {
+        image: localImage,
+        name: 'Typography',
+        count: 2,
+        route: '/sections/elements/typography',
+      },
     ],
   },
 ];
